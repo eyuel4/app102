722292fc1474996bc9ffb2ab2ab79056045e7357
Eyuel Taddese <eyuel_taddese@wgresorts.com>
2019-02-09 22:52:43 -0500

    Update modelscript.sql
    Added OAuth_Client_Details Table

--- modelscript.sql
+++ modelscript.sql
@@ -261,3 +261,17 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+CREATE TABLE oauth_client_details (
+    client_id VARCHAR(256) PRIMARY KEY,
+    resource_ids VARCHAR(256),
+    client_secret VARCHAR(256),
+    scope VARCHAR(256),
+    authorized_grant_types VARCHAR(256),
+    web_server_redirect_uri VARCHAR(256),
+    authorities VARCHAR(256),
+    access_token_validity INTEGER,
+    refresh_token_validity INTEGER,
+    additional_information VARCHAR(4096),
+    autoapprove VARCHAR(256)
+);
